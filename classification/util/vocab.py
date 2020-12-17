7c323beca4aad3cb59badd5c01cb4f49e7388a09
jaeman <woaksths@gmail.com>
2020-12-18 00:28:16 +0900

    extract lexicon using attention mechanism

--- classification/util/vocab.py
+++ classification/util/vocab.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn.functional as F
+from util.lexicon_config import STOP_WORDS, END_WORDS 
 
 def itos(vocab, input_var):
     # input_var -> # batch, seq_len
@@ -13,21 +15,39 @@ def itos(vocab, input_var):
     return result
 
 
-def extract_lexicon(input_vocab, input_variable, target_variable, attn):
-    lexicon = {0:{}, 1:{}}
-    input_text = itos(input_vocab, input_variable)
-    probs, indices = attn.max(-1)
-    
-    for idx, (attn_idx, input_var, label) in enumerate(zip(indices, input_variable, target_variable)):
-        vocab_index = input_var[attn_idx.item()].item()
-        word = input_vocab.itos[vocab_index]
+def itos_sent(vocab, input_var):
+    # input_var -> # batch, seq_len
+    # vocab -> GloVe vector
+    assert input_var.dim() == 1
+    result = []
+    for idx in input_var:
+        result.append(vocab.itos[idx])
+    return ' '.join(result)
+
+
+def extract_lexicon(input_vocab, input_variable, target_variable, logits, attn):
+    lexicon = {0:[], 1:[]}
+    attn_probs, attn_indices = attn.topk(k=10, dim=-1)
+    logits = F.softmax(logits, dim=-1)
+    confidences, indices = logits.max(dim=-1)
+    for idx, (confidence, (pred, label)) in enumerate(zip(confidences, zip(indices, target_variable))):
+        confidence = confidence.item()
+        pred = pred.item()
         label = label.item()
-        if label in lexicon:
-            if word in lexicon[label]:
-                lexicon[label][word] += 1
-            else:
-                lexicon[label][word] = 1
-    return lexicon    
+        if confidence >= 0.9 and pred == label:
+            lexicon_idx = [input_variable[idx][top_idx] for top_idx in attn_indices[idx]]
+            word_set = []
+            for vocab_idx in lexicon_idx:
+                word = input_vocab.itos[vocab_idx.item()] 
+                if word in STOP_WORDS or word in END_WORDS:
+                    continue
+                else:
+                    word_set.append(word)
+            lexicon[label].append(word_set)        
+#             word_set = [input_vocab.itos[vocab_idx.item()] for vocab_idx in lexicon_idx]
+#             lexicon[label].append(word_set)
+#             print('label: {}'.format(label), word_set)
+    return lexicon
 
 
 def merge_lexicon(total_dict, step_dict):
@@ -38,3 +58,5 @@ def merge_lexicon(total_dict, step_dict):
             else:
                 total_dict[label][word] = step_dict[label][word]
     return total_dict
+
+
